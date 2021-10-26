720fbefcf70b0f821ffbb545e324cd5f287ceb26
chris-y <maxsummer256@gmail.com>
2021-10-26 10:53:41 +0900

    [MOD] conform with convention

--- users/utils.py
+++ users/utils.py
@@ -8,8 +8,8 @@ from users.models import User
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
-        if "Authorization" not in request.headers : 
-            return JsonResponse ({"message" : "UNAUTHORIZED"}, status=401)
+        if 'Authorization' not in request.headers : 
+            return JsonResponse ({'message' : 'UNAUTHORIZED'}, status=401)
 
         access_token = request.headers.get('Authorization')
         
