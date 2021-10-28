03dc5c2f057f503d93a0b9b0e545f683c9deae40
song <song7868@gmail.com>
2021-10-28 19:18:01 +0900

    add : 충돌해결1
    add : settings.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls    import path
 
-from products.views import WishList, WishFlag
+from products.views import ProductView, BrandView, DetailProductView, WishList, WishFlag
 
 urlpatterns = [
     path('/wishlist', WishList.as_view()),
-    path('/wishflag', WishFlag.as_view())
+    path('/wishflag', WishFlag.as_view()),
+    path('', ProductView.as_view()),
+    path('/brand', BrandView.as_view()),
+    path('/<int:product_id>', DetailProductView.as_view())
 ]
