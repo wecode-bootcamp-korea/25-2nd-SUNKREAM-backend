a53ec992069e62621abd24d46f8ad236f5a099b8
song <song7868@gmail.com>
2021-12-21 07:15:32 +0900

    [영록]fix : products/viesw.py

--- products/views.py
+++ products/views.py
@@ -3,12 +3,12 @@ from json.decoder import JSONDecodeError
 
 from django.views                   import View
 from django.http                    import JsonResponse
-from django.db.models.query         import Prefetch
+from django.db.models.query     import Prefetch
 from django.db.models               import Q
 from django.db.models.aggregates    import Max, Min
 
 from products.models    import Product, ProductImage, Brand, Wishlist
-from orders.models      import Order
+from orders.models       import Order
 
 from users.utils  import login_decorator
 
@@ -16,7 +16,7 @@ class WishList(View):
     @login_decorator
     def post(self, request):
         try:
-            user_id      = request.user.id
+            user_id           = request.user.id
             wish_product = request.GET.get('product_id')
 
             if not Product.objects.filter(id = wish_product).exists():
@@ -48,16 +48,16 @@ class WishList(View):
             wish_list = Wishlist.objects.filter(user_id = user_id)
 
             results = [{
-                'id'    : wish.product.id,
-                'brand' : wish.product.brand.name,
-                'name'  : wish.product.name,
-                'price' : wish.product.release_price,
-                'image' : 
-                [
-                    {
-                        'thumbnail' : img.image_url
-                    } for img in wish.product.productimage_set.all()[:1]]
-                } for wish in wish_list]
+                            'id'        : wish.product.id,
+                            'brand'  : wish.product.brand.name,
+                            'name'  : wish.product.name,
+                            'price'   : wish.product.release_price,
+                            'image' :  [
+                                                {
+                                                    'thumbnail' : img.image_url
+                                                } for img in wish.product.productimage_set.all()[:1]
+                                            ]
+                            } for wish in wish_list]
 
             return JsonResponse({'results' : results}, status = 200)
 
@@ -68,7 +68,7 @@ class WishFlag(View):
     @login_decorator
     def get(self, request):
         try:
-            user_id      = request.user.id
+            user_id           = request.user.id
             wish_product = request.GET.get('product_id')
 
             if not Product.objects.filter(id = wish_product):
@@ -82,8 +82,8 @@ class WishFlag(View):
                 check_my_wish = False
 
             results = {
-                'wish_count'    : wish_count,
-                'check_my_wish' : check_my_wish,
+                'wish_count'        : wish_count,
+                'check_my_wish'  : check_my_wish,
             }
 
             return JsonResponse({'results' : results}, status = 200)
@@ -95,19 +95,19 @@ class ProductView(View) :
     def get(self, request) :
         try :
             brand_id    = request.GET.getlist('brand_id')
-            size_id     = request.GET.getlist('size_id')
-            sort        = request.GET.get('sort')
-            search      = request.GET.get('search')
-            start       = int(request.GET.get('start',0))
-            stop        = int(request.GET.get('stop',0))
-            limit       = int(request.GET.get('limit', 40))
-            offset      = int(request.GET.get('offset', 0))
+            size_id       = request.GET.getlist('size_id')
+            sort           = request.GET.get('sort')
+            search       = request.GET.get('search')
+            start          = int(request.GET.get('start',0))
+            stop          = int(request.GET.get('stop',0))
+            limit          = int(request.GET.get('limit', 40))
+            offset       = int(request.GET.get('offset', 0))
 
-            sort_by = {
-                        'now_buy_price'  : '-buy_price',
-                        'now_sell_price' : 'sell_price',
-                        'premium'        : 'release_price',
-                      }
+            sort_by     = {
+                                    'now_buy_price' : '-buy_price',
+                                    'now_sell_price'  : 'sell_price',
+                                    'premium'          : 'release_price',
+                                }
 
             products_filter = Q()
             
@@ -123,26 +123,26 @@ class ProductView(View) :
             buy_price_filter = (Q(productsize__bidding__bidding_position_id = 2) & Q(productsize__bidding__bidding_status_id = 1))
             sell_price_filter   = (Q(productsize__bidding__bidding_position_id = 1) & Q(productsize__bidding__bidding_status_id = 1))
 
-            products = Product.objects \
+            products = Product.objects\
                                         .annotate(buy_price = Min('productsize__bidding__price', filter = buy_price_filter),
-                                                sell_price = Max('productsize__bidding__price', filter = sell_price_filter)) \
+                                                         sell_price = Max('productsize__bidding__price', filter = sell_price_filter)) \
                                         .filter(products_filter) \
                                         .select_related('brand') \
                                         .order_by(sort_by.get(sort,'id')) \
                                         .prefetch_related(Prefetch('productimage_set', queryset = ProductImage.objects.all().order_by('id')))
             
             if limit :
-                limit    = offset + limit
-                products = products[offset : limit]
+                        limit    = offset + limit
+                        products = products[offset : limit]
 
             products_list = [{
-                                'id'            : product.id,
-                                'brand'         : product.brand.name,
-                                'name'          : product.name,
-                                'thumbnail_url' : product.productimage_set.first().image_url,
-                                'product_price' : product.buy_price,
-                                'release_price' : product.release_price
-                            } for product in products]
+                                        'id'                    : product.id,
+                                        'brand'              : product.brand.name,
+                                        'name'              : product.name,
+                                        'thumbnail_url' : product.productimage_set.first().image_url,
+                                        'product_price' : product.buy_price,
+                                        'release_price'  : product.release_price
+                                    } for product in products]
             
             return JsonResponse({'products_list' : products_list}, status = 200)
 
@@ -157,16 +157,16 @@ class BrandView(View):
             brands = Brand.objects.all()
 
             brand_list = [{
-                            'brand_id'   : brand.id,
-                            'brand_name' : brand.name,
-                        } for brand in brands]
+                                    'brand_id'   : brand.id,
+                                    'brand_name' : brand.name,
+                            } for brand in brands]
 
             return JsonResponse({'brand_list' : brand_list}, status = 200)
 
 class DetailProductView(View) :
     def get(self, request, product_id) :
         try :    
-            buy_price_filter    = (Q(productsize__bidding__bidding_position_id = 2) & Q(productsize__bidding__bidding_status_id = 1))
+            buy_price_filter  = (Q(productsize__bidding__bidding_position_id = 2) & Q(productsize__bidding__bidding_status_id = 1))
             sell_price_filter   = (Q(productsize__bidding__bidding_position_id = 1) & Q(productsize__bidding__bidding_status_id = 1))
 
             if not Product.objects.filter(id = product_id).exists():
@@ -174,23 +174,24 @@ class DetailProductView(View) :
             
             product               = Product.objects \
                                                     .annotate(buy_price = Min('productsize__bidding__price', filter = buy_price_filter), 
-                                                              sell_price = Max('productsize__bidding__price', filter = sell_price_filter)) \
+                                                                    sell_price = Max('productsize__bidding__price', filter = sell_price_filter)) \
                                                     .get(id = product_id)
+                                                    
             orders                = Order.objects.filter(bidding__product_size__product_id = product_id).order_by('-created_at')
             wishlist              = len(Wishlist.objects.filter(id=product_id).all())
             
             product_detail = [{
-                                'product_id'      : product.id,
-                                'name'            : product.name,
-                                'brand_name'      : product.brand.name,
-                                'release_price'   : product.release_price,
-                                'model_number'    : product.model_number,
-                                'image_list'      : [image.image_url for image in product.productimage_set.all()],
-                                'recent_price'    : orders.first().bidding.price if orders.exists() else None,
-                                'buy_price'       : product.buy_price,
-                                'sell_price'      : product.sell_price,
-                                'total_wishlist'  : wishlist,
-                            }]
+                                            'product_id'      : product.id,
+                                            'name'            : product.name,
+                                            'brand_name'      : product.brand.name,
+                                            'release_price'   : product.release_price,
+                                            'model_number'    : product.model_number,
+                                            'image_list'      : [image.image_url for image in product.productimage_set.all()],
+                                            'recent_price'    : orders.first().bidding.price if orders.exists() else None,
+                                            'buy_price'       : product.buy_price,
+                                            'sell_price'      : product.sell_price,
+                                            'total_wishlist'  : wishlist,
+                                      }]
 
             return JsonResponse({'product_detail' : product_detail}, status=200)
 
